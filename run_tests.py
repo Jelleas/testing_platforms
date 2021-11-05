3bb70634cc6ffda363b2a25326416056912db24c
Jelle van Assema <jelle.van.assema@gmail.com>
2021-11-05 15:19:15 +0100

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -4,16 +4,17 @@ import subprocess
 def get_submissions():
     return sorted([int(dir_name) for dir_name in os.listdir("submissions") if not dir_name.startswith(".")])
 
-for submission in get_submissions():
-    submission_path = f"submissions/{submission}/cash.py"
-    output_path = f"outputs/{submission}.txt"
+if __name__ == "__main__":
+    for submission in get_submissions():
+        submission_path = f"submissions/{submission}/cash.py"
+        output_path = f"outputs/{submission}.txt"
 
-    if not os.path.exists("outputs"):
-        os.mkdir("outputs")
+        if not os.path.exists("outputs"):
+            os.mkdir("outputs")
 
-    with open(output_path, "w") as f:
-        print(f"Testing - {submission_path} => {output_path}", end="", flush=True)
-        
-        exit_status = subprocess.call(["pytest", "--path", submission_path], stdout=f)
+        with open(output_path, "w") as f:
+            print(f"Testing - {submission_path} => {output_path}", end="", flush=True)
 
-        print(f"{'  ' if submission < 10 else ''} | {'FAILED' if exit_status else 'SUCCESS'}")
+            exit_status = subprocess.call(["pytest", "--path", submission_path], stdout=f)
+
+            print(f"{'  ' if submission < 10 else ''} | {'FAILED' if exit_status else 'SUCCESS'}")
